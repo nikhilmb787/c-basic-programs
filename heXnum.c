25b1cc3ace804e2f356bccb3294a3a1679e1f849
Nikhil M B <nikhilaz787@gmail.com>
2023-11-28 12:56:19 +0530

    Hexadecimal printing program added

---
+++ heXnum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+	
+	int num;
+
+	printf("Enter the number: ");
+	scanf("%d", &num);
+
+	printf("\nHexadecimal of %d = %X",num, num);  // %X prints hexadecimal of the number 
+
+
+}
