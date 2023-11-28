8918711cb0407712f5e6eacde07c3f606bb6645b
Nikhil M B <nikhilaz787@gmail.com>
2023-11-28 22:59:59 +0530

    Program to clear a particular bit of a number

---
+++ clearAbit.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main(){
+
+	int num, pos;
+
+	printf("\nEnter the number: "); 
+	scanf("%d", &num);
+
+	printf("Enter the pos: ");
+	scanf("%d", &pos);
+
+	printf("Before setting bit : %d ", num);
+
+	num = num & ~ (1 << pos);  
+
+	printf("\nAfter setting bit: %d  ", num);
+
+
+}
