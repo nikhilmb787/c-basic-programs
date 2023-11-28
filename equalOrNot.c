009d066c40c3014b6b73f7d5feebb9cab5dc6108
Nikhil M B <nikhilaz787@gmail.com>
2023-11-28 23:21:11 +0530

    Program to check the given 2 numbers are equal or not

---
+++ equalOrNot.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+
+	int num1, num2;
+
+	printf("\nEnter the number1 : ");
+	scanf("%d", &num1);
+
+	printf("\nEnter the number2 : ");
+	scanf("%d", &num2);
+
+	num1 == num2 ? printf("\nnumbers are equal \n") : printf("\nnumbers are not equal\n");
+
+}
